6a861488a8317389a23e7b25630058ae1adb5f1c
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-07-04 18:03:22 +0900

    PLA

--- src/Circuit.hs
+++ src/Circuit.hs
@@ -2,7 +2,7 @@
 
 module Circuit (
 	Circuit, makeCircuit, step, setBits, peekOWire, bitsToWord, wordToBits,
-	CircuitBuilder, connectWire, connectWire64, delay,
+	CircuitBuilder, connectWire, connectWire64, connectWire0, delay,
 		constGate, idGate, notGate, andGate, orGate, triGate,
 	IWire, OWire, Bits(..), BitLen, BitPosIn, BitPosOut,
 	setMultBits
@@ -14,6 +14,9 @@ import CircuitCore
 
 connectWire64 :: OWire -> IWire -> CircuitBuilder ()
 connectWire64 o i = connectWire (o, 64, 0) (i, 64, 0)
+
+connectWire0 :: OWire -> IWire -> CircuitBuilder ()
+connectWire0 o i = connectWire (o, 1, 0) (i, 1, 0)
 
 setMultBits :: [IWire] -> [Word64] -> Circuit -> Circuit
 setMultBits is vs = foldr (.) id $ zipWith setBits is (wordToBits <$> vs)
