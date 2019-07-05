385993f787954860acb9a3870fcb38c2978c8dd0
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-07-05 16:16:48 +0900

    ALU: Overflow

--- src/Samples/Alu.hs
+++ src/Samples/Alu.hs
@@ -6,7 +6,7 @@ import Circuit
 import Element
 import Samples.CarryLookahead2
 
-alu :: CircuitBuilder (IWire, IWire, IWire, IWire, IWire, IWire, OWire)
+alu :: CircuitBuilder (IWire, IWire, IWire, IWire, IWire, IWire, OWire, OWire)
 alu = do
 	(ain, aout) <- idGate
 	(bin, bout) <- idGate
@@ -23,7 +23,7 @@ alu = do
 
 	(aa, ab, ao) <- andGate
 	(oa, ob, oo) <- orGate
-	(ci, ca, cb, cs, _co) <- carries
+	(ci, ca, cb, cs, co) <- carries
 	(xc, xa, xb, xo) <- xorGate3
 	(op, a, o, s, r) <- mux3
 	connectWire64 maout `mapM_` [aa, oa, ca, xa]
@@ -32,4 +32,8 @@ alu = do
 	connectWire64 ao a
 	connectWire64 oo o
 	connectWire64 xo s
-	return (ainv, binv, op, ci, ain, bin, r)
+
+	(ofci, ofco, ovfl) <- xorGate
+	connectWire (cs, 1, 63) (ofci, 1, 0)
+	connectWire0 co ofco
+	return (ainv, binv, op, ci, ain, bin, r, ovfl)
