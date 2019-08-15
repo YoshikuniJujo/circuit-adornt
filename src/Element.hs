ee820db8ce29b8037d01c719fc68c09f77f4e841
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-15 11:11:17 +0900

    move pla8 to circuit-adornt-parts

--- src/Element.hs
+++ src/Element.hs
@@ -7,18 +7,10 @@ module Element (
 	dlatch, dflipflop
 	) where
 
-import Control.Arrow
-import Control.Monad
 import Data.Foldable
-import Data.List
-import Data.Bool
-import Data.Word
-
-import Circuit.Adornt.Parts
 
 import Circuit
-
-import qualified Data.Bits as B
+import Circuit.Adornt.Parts
 
 testZero :: CircuitBuilder (IWire, OWire)
 testZero = do
@@ -32,55 +24,3 @@ testZero = do
 	cz <- constGate $ Bits 0
 	connectWire (cz, 63, 0) (oin, 63, 1)
 	return (iin, oout)
-
-data Bit = O | I deriving (Show, Eq)
-
-pla8 :: [(Word8, Word8)] -> CircuitBuilder (IWire, OWire)
-pla8 tbl_ = do
-	(iin, iout) <- idGate
-	(iss, outs) <- plaGen 8 tbl
-	(oin, oout) <- idGate
-	connectWireOutToIns iout iss
-	connectWireOutsToIn outs oin
-	z <- constGate $ Bits 0
-	connectWire (z, 56, 0) (oin, 56, 8)
-	return (iin, oout)
-	where
-	tbl = (numToBits 8 *** numToBits 8) <$> tbl_
-	numToBits :: B.Bits n => Word8 -> n -> [Bit]
-	numToBits c n = map (bool O I . B.testBit n) [0 .. fromIntegral c - 1]
-	connectWireOutToIns o is = zipWithM_ (\n i -> connectWire (o, 1, n) (i, 1, 0)) [0 ..] is
-	connectWireOutsToIn os i = zipWithM_ (\n o -> connectWire (o, 1, 0) (i, 1, n)) [0 ..] os
-
-plaGen :: Word8 -> [([Bit], [Bit])] -> CircuitBuilder ([IWire], [OWire])
-plaGen n_ tbl = do
-	(iins, iouts) <- unzip <$> n `replicateM` idGate
-	rs <- pla1 iouts `mapM` trSep tbl
-	return (iins, rs)
-	where
-	n = fromIntegral n_
-	trSep = transpose . map sepSnd
-	sepSnd (x, ys) = zip (repeat x) ys
-
-pla1 :: [OWire] -> [([Bit], Bit)] -> CircuitBuilder OWire
-pla1 iouts tbl1 = do
-	fbs <- frbk0 `mapM` iouts
-	let	ws = map (zipWith bitIndex fbs) ons
-	r <- allOr =<< mapM allAnd ws
-	return r
-	where
-	ons = fst <$> filter ((== I) . snd) tbl1
-	allAnd os = do
-		(as, o) <- multiple andGate . fromIntegral $ length os
-		zipWithM_ connectWire64 os as
-		return o
-	allOr os = do
-		(xs, o) <- multiple orGate . fromIntegral $ length os
-		zipWithM_ connectWire64 os xs
-		return o
-	bitIndex (x, _) O = x
-	bitIndex (_, y) I = y
-	frbk0 o = do
-		(ni, no) <- notGate
-		connectWire0 o ni
-		return  (no, o)
