ac7425bb74dbfbd9f44bcdf6353547ef625d8398
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-15 09:52:32 +0900

    move srlatch, dlatch and dflipflop to circuit-adornt-parts

--- src/Element.hs
+++ src/Element.hs
@@ -29,39 +29,6 @@ testZero = do
 	connectWire (cz, 63, 0) (oin, 63, 1)
 	return (iin, oout)
 
-srlatch :: CircuitBuilder Wire22
-srlatch = do
-	(r, nqin, qout) <- norGate
-	(s, qin, nqout) <- norGate
-	connectWire64 nqout nqin
-	connectWire64 qout qin
-	return (r, s, qout, nqout)
-
-dlatch :: CircuitBuilder Wire22
-dlatch = do
-	(cin, cout) <- idGate
-	(din, dout) <- idGate
-	(cr, dr, rout) <- andNotBGate
-	(cs, ds, sout) <- andGate
-	(r, s, q, nq) <- srlatch
-	connectWire0_64 cout `mapM_` [cr, cs]
-	connectWire64 dout `mapM_` [dr, ds]
-	connectWire64 rout r
-	connectWire64 sout s
-	return (cin, din, q, nq)
-
-dflipflop :: CircuitBuilder Wire22
-dflipflop = do
-	(cin, cout) <- idGate
-	(ni, no) <- notGate
-	connectWire0 cout ni
-	(cm, dm, qm, _nqm) <- dlatch
-	(cs, ds, qs, nqs) <- dlatch
-	connectWire0 cout cm
-	connectWire0 no cs
-	connectWire64 qm ds
-	return (cin, dm, qs, nqs)
-
 data Bit = O | I deriving (Show, Eq)
 
 numToBits :: B.Bits n => Word8 -> n -> [Bit]
