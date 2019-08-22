407767b5a4c7f46b423a553d3fdacb5367dc0ae2
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2019-08-22 09:49:59 +0900

    module Circuit.Adornt

---
+++ src/Circuit/Adornt.hs
@@ -0,0 +1,59 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module Circuit.Adornt (
+
+-- * Simulator
+
+-- ** Circuit Simulator
+CircuitSimulator, prepareSimulator, prepareSimulatorRandom, prepareSimulatorRandomIO, step,
+
+-- ** Set Bits of Input Wire
+IWire, setBits, setMultiBits,
+
+-- ** Peek Bits of Output wire
+OWire, peekOWire, peekMultiOWires,
+
+-- ** Convert between Bits and Word64
+Bits, bitsToWord, wordToBits,
+
+-- * Diagram
+renderSVG, mkWidth, mkHeight, dims2D,
+circuitDiagram,
+
+-- * Builder
+
+-- ** Circuit Builder
+CircuitBuilder,
+
+-- ** Basic Gates
+BasicGate, constGate, idGate, andGate, orGate, notGate, triGate, cheatGate,
+
+-- ** Delay
+delay,
+
+-- ** Wire Sets
+Wire11, Wire21, Wire31, Wire41, Wire51, Wire22,
+
+-- ** Wire Connection
+connectWire, connectWire0, connectWire64, connectWire0_64,
+BitLen, BitPosIn, BitPosOut,
+
+-- ** Named Block
+putNamedBlock, BlockName,
+
+) where
+
+import Diagrams.Prelude
+
+import Circuit.Adornt.Simulator
+import Circuit.Adornt.Builder
+import Circuit.DiagramDsl
+
+import qualified Circuit.Adornt.Diagram as D
+import qualified Diagrams.Backend.SVG as S
+
+circuitDiagram :: Int -> CircuitBuilder [OWire] -> Either String (Diagram S.B)
+circuitDiagram n = either Left (Right . drawDiagram) . D.circuitDiagram n
+
+renderSVG :: FilePath -> SizeSpec V2 Double -> Diagram S.B -> IO ()
+renderSVG = S.renderSVG
